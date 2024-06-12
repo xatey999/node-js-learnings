2d5e010f732eb11cd7f2bca1efc4b23437f46dac
Ujjwal Adhikari <pokhreli.thito10@gmail.com>
2024-06-12 13:44:59 +0545

    Learning some basic javascripts

---
+++ filter/filter.js
@@ -0,0 +1,7 @@
+const arr = [1,2,"3",4,5,6,7,8];
+
+var fill = arr.filter((item)=>{
+    return item===3
+})
+
+console.log(fill);
---
+++ learning/app.js
@@ -0,0 +1,6 @@
+console.log('Hello world!');
+
+var x = 30;
+if(x==="30"){
+    console.log("Matched:");
+} else console.log("Type not matched:");
